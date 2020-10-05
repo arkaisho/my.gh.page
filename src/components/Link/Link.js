9e91a1b92466e518210c65eb2003cb0ea79fd856
arkaisho <joaopedro10782@gmail.com>
2020-10-05 19:17:29 -0300

    visual changes

--- src/components/Link/Link.js
+++ src/components/Link/Link.js
@@ -5,7 +5,7 @@ export default class Link extends React.Component{
     render(){
         return(
             <div style={{height:"20px"}}>
-                <img className="icon" alt="icon" src={this.props.icon}/><a href={this.props.info.link}>{this.props.info.text}</a>
+                <img className="icon" alt="icon" src={this.props.icon}/><a className="link" href={this.props.info.link}>{this.props.info.text}</a>
             </div>
         );
     }
