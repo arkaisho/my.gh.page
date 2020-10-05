7e281256738ceb7423a40ba4bc041cb2233a2b53
arkaisho <joaopedro10782@gmail.com>
2020-10-05 18:16:08 -0300

    Updates

---
+++ precache-manifest.91f2e0ee56ca921c2e527d18b28495aa.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d32533c640c06ef6e1455b1a1144dd6",
+    "url": "/my.gh.page/index.html"
+  },
+  {
+    "revision": "264627bb02cd38ca04a0",
+    "url": "/my.gh.page/static/css/main.dcc5d813.chunk.css"
+  },
+  {
+    "revision": "7af6b7c34da6f24d66db",
+    "url": "/my.gh.page/static/js/2.3da080ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my.gh.page/static/js/2.3da080ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "264627bb02cd38ca04a0",
+    "url": "/my.gh.page/static/js/main.4fb442b2.chunk.js"
+  },
+  {
+    "revision": "e057a12bb8f54a8dc73c",
+    "url": "/my.gh.page/static/js/runtime-main.4808754f.js"
+  },
+  {
+    "revision": "ac8a614d606b44cdb93a84ea3dcf1a6f",
+    "url": "/my.gh.page/static/media/ConfigCondensed-Bold.ac8a614d.otf"
+  },
+  {
+    "revision": "6a7b82496f639afa3fe6d31188e9844d",
+    "url": "/my.gh.page/static/media/ConfigCondensed-Light.6a7b8249.otf"
+  },
+  {
+    "revision": "1b9b9f71269e504156ce9d89a00f2551",
+    "url": "/my.gh.page/static/media/github.1b9b9f71.png"
+  },
+  {
+    "revision": "518048d1e91b835dd357f67baf46f8eb",
+    "url": "/my.gh.page/static/media/medium.518048d1.png"
+  },
+  {
+    "revision": "5863d4253ad20e14b0a596bd423cb1b5",
+    "url": "/my.gh.page/static/media/roundprofilepic.5863d425.png"
+  }
+]);
