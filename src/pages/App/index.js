1e10fa43f4f78a0166e3bfbe7b5b4cb91d865215
arkaisho <joaopedro10782@gmail.com>
2020-10-08 08:15:54 -0300

    responsivity improvement

--- src/pages/App/index.js
+++ src/pages/App/index.js
@@ -1,43 +1,53 @@
-import React from 'react';
+import React from "react";
 import ProfilePic from "../../components/ProfilePic/ProfilePic";
 import Link from "../../components/Link/Link";
 import github from "../../assets/icons/github.png";
 import linkedin from "../../assets/icons/linkedin.png";
 import medium from "../../assets/icons/medium.png";
-import './style.css'
+import "./style.css";
 
 let dados = {
-  github : {
-    link:"https://www.github.com/arkaisho",
-    text:"GitHub"
+  github: {
+    link: "https://www.github.com/arkaisho",
+    text: "GitHub",
   },
-  linkedin : {
-    link:"https://www.linkedin.com/in/joao-pedro-estevam/",
-    text:"LinkedIn"
+  linkedin: {
+    link: "https://www.linkedin.com/in/joao-pedro-estevam/",
+    text: "LinkedIn",
   },
-  medium : {
-    link:"https://medium.com/@joaopedro10782",
-    text:"Medium"
-  }
-}
+  medium: {
+    link: "https://medium.com/@joaopedro10782",
+    text: "Medium",
+  },
+};
 
-const appStyle={position:"absolute",height:"100%",display:"flex",flexDirection:"row",alignItems:"center",marginLeft:"10%"}
+const appStyle = {
+  position: "absolute",
+  height: "100%",
+  display: "flex",
+  flexDirection: "row",
+  alignItems: "center",
+  marginLeft: "10%",
+};
 
-export default class App extends React.Component{
-  render(){
+export default class App extends React.Component {
+  render() {
     return (
       <div style={appStyle}>
-          <div>
-            <ProfilePic/>
-            <div className="information" >
-              <h2 className="profile-name">João Pedro</h2>
-              <div style={{marginLeft:"10px"}}>
-                <Link icon={github} info={dados.github}/><br/>
-                <Link icon={linkedin} info={dados.linkedin} /><br/>
-                <Link icon={medium} info={dados.medium} /><br/>
-              </div>
+        <div className="profile-container">
+          <ProfilePic />
+          <div className="information">
+            <h2 className="profile-name">João Pedro</h2>
+            <div className="link-list">
+              <Link icon={github} info={dados.github} />
+              <br />
+              <Link icon={linkedin} info={dados.linkedin} />
+              <br />
+              <Link icon={medium} info={dados.medium} />
+              <br />
             </div>
           </div>
+        </div>
       </div>
     );
   }
